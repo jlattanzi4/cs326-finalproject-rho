d36a936575722facb0289225aa7921e18d0e5f1d
jlattanzi4 <lattanzi.joseph@gmail.com>
2020-05-03 15:42:09 -0400

    fixing url

--- html/register.js
+++ html/register.js
@@ -9,7 +9,7 @@ function userCreate() {
 	let password = document.getElementById("password").value;
 	
 	const data = {'username' : userName, 'firstName': firstName, 'lastName': lastName, 'email': email, "password": password};
-	const newURL = url + "register.html/users/" + userName + "/create";
+	const newURL = url + "/users/" + userName + "/create";
 	console.log("userCreate: fetching " + newURL);
 	const resp = await postData(newURL, data); 
 	const j = await resp.json();
